6d65e54f7a1162784496c53d0966e28847b5cd6e
aminSimyari <149312884+aminSimyari@users.noreply.github.com>
2025-08-19 15:39:13 +0330

    added li & input

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ function addTask() {
   let text = input.value.trim();
 
   if (text === "") return; // do nothing if empty
+
+  // create li
+  let li = document.createElement("li");
+  li.innerHTML = text + " <button onclick='removeTask(this)'>Delete</button>";
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = ""; // clear input
+}
